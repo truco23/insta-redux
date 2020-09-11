152c7fc489a081d4e2609e3f7500c98a55a4d89c
G. Moraes Brasil <g.moraes.brasil@avanade.com>
2020-09-10 21:47:04 -0300

    Updates

---
+++ precache-manifest.1e1ff970b567e924ea0b02169c6a9fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d96c383119bd87155bc8e49d3b50a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b60b9bf5178721cabf2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c2c268ba9c7fc8786b3",
+    "url": "/static/js/2.f62e1321.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f62e1321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b60b9bf5178721cabf2",
+    "url": "/static/js/main.8cbe2038.chunk.js"
+  },
+  {
+    "revision": "696afa0cfeb734f4025d",
+    "url": "/static/js/runtime-main.d9ec8eab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
